df4dc04633ccd7c888331733fe4e27ddb8e1bb02
Andrew Danner <adanner@cs.swarthmore.edu>
2018-06-14 16:52:47 -0400

    qogl helper not needed...for now

--- qoglHelper.h
+++ qoglHelper.h
@@ -1,5 +1,8 @@
 #pragma once
-
+/* Nuclear option if Qt gives me any guff about
+   including gl* functions in a class with Qt code.
+   Just inherit this helper */
+   
 #include <QOpenGLFunctions_4_1_Core>
 
 class QOGLHelper : protected QOpenGLFunctions_4_1_Core{
