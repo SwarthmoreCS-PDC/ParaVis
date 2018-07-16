19a14313747f2e947d3db8bbe3ed430306944ddb
Andrew Danner <adanner@cs.swarthmore.edu>
2018-07-16 15:29:14 -0400

    make cude detect arch more flexible

--- cmake_modules/detectCUDAarch.cmake
+++ cmake_modules/detectCUDAarch.cmake
@@ -1,8 +1,13 @@
 if(NOT QVis_CUDA_ARCH)
   set(CUDA_TEST_SRC ${CMAKE_SOURCE_DIR}/test/detect_cuda_arch.cu)
   set(CUDA_TEST_OUT ${CMAKE_BINARY_DIR}/detect_cuda_arch)
+  set(CUDA_CMD ${CMAKE_CUDA_COMPILER} -Wno-deprecated-gpu-targets -lcuda)
+  if(CMAKE_CUDA_HOST_COMPILER)
+    set(CUDA_CMD ${CUDA_CMD} -ccbin ${CMAKE_CUDA_HOST_COMPILER})
+  endif()
+  message(STATUS "${CUDA_CMD}")
   execute_process(COMMAND
-    nvcc -Wno-deprecated-gpu-targets -lcuda
+    ${CUDA_CMD}
     ${CUDA_TEST_SRC} -o ${CUDA_TEST_OUT})
   execute_process(COMMAND ${CUDA_TEST_OUT}
                     RESULT_VARIABLE CUDA_RETURN_CODE
