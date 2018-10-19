87b1ec491b40fe4a8039f654020c4976b5fb9135
Andrew Danner <adanner@cs.swarthmore.edu>
2018-10-19 13:37:10 -0400

    format

--- src/pthreads/pthreadGridVisiWrapper.cpp
+++ src/pthreads/pthreadGridVisiWrapper.cpp
@@ -1,10 +1,9 @@
 #include "pthreadGridVisiWrapper.h"
-#include "gridVisiWrapper.h"
 #include "cvisi_struct.h"
-#include <new>      //for bad_alloc
+#include "gridVisiWrapper.h"
+#include <cstdio>   //for perror
 #include <iostream> // for cerr
-#include <cstdio>  //for perror
-
+#include <new>      //for bad_alloc
 
 /* initialize the visualization: only call once (by one thread)
  *
@@ -15,26 +14,25 @@
  *     iters: run for specified number of iterations, or if 0 run forever
  *     returns: a pthread_visi_handle or NULL on error
  */
-extern "C" visi_handle init_pthread_animation(int num_tids, int rows,
-                                                      int cols, char *name,
-                                                      int iters) {
+extern "C" visi_handle init_pthread_animation(int num_tids, int rows, int cols,
+                                              char *name, int iters) {
 
   visi_handle handle = nullptr; // pointer to pthread_visi_struct
-  PthreadGridVisiWrapper* anim = nullptr;
-  GridVisiWrapper* app = nullptr;
+  PthreadGridVisiWrapper *anim = nullptr;
+  GridVisiWrapper *app = nullptr;
 
-    try{
-  handle = new visi_struct;
-  app = new GridVisiWrapper(name);
-  anim = new PthreadGridVisiWrapper(num_tids, rows, cols);
+  try {
+    handle = new visi_struct;
+    app = new GridVisiWrapper(name);
+    anim = new PthreadGridVisiWrapper(num_tids, rows, cols);
 
-  //*TODO: fix possible race condition?
-  //handle->app->setAnimation(anim); //currently causes freezing
-  // */
-} catch (std::bad_alloc& ba) {
-  std::cerr << "bad_alloc caught: " << ba.what() << '\n';
-  return nullptr;
-}
+    //*TODO: fix possible race condition?
+    // handle->app->setAnimation(anim); //currently causes freezing
+    // */
+  } catch (std::bad_alloc &ba) {
+    std::cerr << "bad_alloc caught: " << ba.what() << '\n';
+    return nullptr;
+  }
   handle->app = app;
   handle->visi_grid = anim;
   return handle;
@@ -54,7 +52,4 @@ PthreadGridVisiWrapper::PthreadGridVisiWrapper(int ntids, int r, int c)
 PthreadGridVisiWrapper::~PthreadGridVisiWrapper() { /* do nothing ? */
 }
 
-void PthreadGridVisiWrapper::update() {
-
-  pthread_barrier_wait(&m_barrier);
-}
+void PthreadGridVisiWrapper::update() { pthread_barrier_wait(&m_barrier); }
